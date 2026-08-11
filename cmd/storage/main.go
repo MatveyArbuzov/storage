480b2909a196e12410e5660460d472c899a0339a
Arbuzov Matvey <arbuzov.matv.y@yandex.ru>
2026-08-11 16:11:31 +0400

    Add simple upload method

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/matveyarbuzov/storage/internal/storage"
 )
 
 func main() {
 	st := storage.NewStorage()
+	file, err := st.Upload("test.txt", []byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println("it's work!", st)
+	fmt.Println("it uploaded", file)
 }
